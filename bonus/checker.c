fbedab7f6340f54df13f01f91e6fe5b7ca26e2cc
Mohammed El-Aslaouy <mel-asla@e1r5p9.1337.ma>
2025-12-23 01:57:16 +0100

    .

--- bonus/checker.c
+++ bonus/checker.c
@@ -6,49 +6,92 @@
 /*   By: mel-asla <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/21 18:21:00 by mel-asla          #+#    #+#             */
-/*   Updated: 2025/12/14 22:21:00 by mel-asla         ###   ########.fr       */
+/*   Updated: 2025/12/23 01:40:35 by mel-asla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "checker.h"
 
-static void	execute_instruction(char *line, t_list **a, t_list **b)
+static int	ft_strcmp(char *s1, char *s2)
 {
+	int	i;
+
+	i = 0;
+	while (s1[i] || s2[i])
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
+
+static int	valid_instra(char *line)
+{
+	if (!ft_strcmp(line, "ss\n") || !ft_strcmp(line, "sa\n"))
+		return (1);
+	else if (!ft_strcmp(line, "sb\n") || !ft_strcmp(line, "pa\n"))
+		return (1);
+	else if (!ft_strcmp(line, "ra\n") || !ft_strcmp(line, "pb\n"))
+		return (1);
+	else if (!ft_strcmp(line, "rb\n") || !ft_strcmp(line, "rr\n"))
+		return (1);
+	else if (!ft_strcmp(line, "rrb\n") || !ft_strcmp(line, "rra\n"))
+		return (1);
+	else if (!ft_strcmp(line, "rrr\n"))
+		return (1);
+	else
+		return (0);
+}
+
+/*
+sa\n  = 's'(115) + 'a'(97)  + 10 = 222
+sb\n  = 's'(115) + 'b'(98)  + 10 = 223
+ss\n  = 's'(115) + 's'(115) + 10 = 240
+pa\n  = 'p'(112) + 'a'(97)  + 10 = 219
+pb\n  = 'p'(112) + 'b'(98)  + 10 = 220
+ra\n  = 'r'(114) + 'a'(97)  + 10 = 221
+rb\n  = 'r'(114) + 'b'(98)  + 10 = 222 + 100 = 320
+rr\n  = 'r'(114) + 'r'(114) + 10 = 238
+rra = 'r'(114) + 'r'(114) + 'a'(97) = 325 no need to add the 10 
+rrb = 'r'(114) + 'r'(114) + 'b'(98) = 326
+rrr = 'r'(114) + 'r'(114) + 'r'(114) = 342
+*/
+
+static void	line_sum(int sum, t_list **a, t_list **b)
+{
+	if (sum == 222 || sum == 240)
+		ft_swap(a);
+	if (sum == 223 || sum == 240)
+		ft_swap(b);
+	if (sum == 219)
+		ft_push(b, a);
+	if (sum == 220)
+		ft_push(a, b);
+	if (sum == 221 || sum == 238)
+		ft_rotate(a);
+	if (sum == 1337 || sum == 238)
+		ft_rotate(b);
+	if (sum == 325 || sum == 342)
+		ft_reverse_rotate(a);
+	if (sum == 326 || sum == 342)
+		ft_reverse_rotate(b);
+}
+
+static void	do_instruction(char *line, t_list **a, t_list **b)
+{
+	int	sum;
+
 	if (!line)
 		return ;
-	if (line[0] == 's' && line[1] == 'a' && line[2] == '\n')
-		ft_swap(a);
-	else if (line[0] == 's' && line[1] == 'b' && line[2] == '\n')
-		ft_swap(b);
-	else if (line[0] == 's' && line[1] == 's' && line[2] == '\n')
-	{
-		ft_swap(a);
-		ft_swap(b);
-	}
-	else if (line[0] == 'p' && line[1] == 'a' && line[2] == '\n')
-		ft_push(b, a);
-	else if (line[0] == 'p' && line[1] == 'b' && line[2] == '\n')
-		ft_push(a, b);
-	else if (line[0] == 'r' && line[1] == 'a' && line[2] == '\n')
-		ft_rotate(a);
-	else if (line[0] == 'r' && line[1] == 'b' && line[2] == '\n')
-		ft_rotate(b);
-	else if (line[0] == 'r' && line[1] == 'r' && line[2] == '\n')
-	{
-		ft_rotate(a);
-		ft_rotate(b);
-	}
-	else if (line[0] == 'r' && line[1] == 'r' && line[2] == 'a' && line[3] == '\n')
-		ft_reverse_rotate(a);
-	else if (line[0] == 'r' && line[1] == 'r' && line[2] == 'b' && line[3] == '\n')
-		ft_reverse_rotate(b);
-	else if (line[0] == 'r' && line[1] == 'r' && line[2] == 'r' && line[3] == '\n')
-	{
-		ft_reverse_rotate(a);
-		ft_reverse_rotate(b);
-	}
-	else
+	if (!valid_instra(line))
 		error_exit();
+	sum = line[0];
+	sum += line[1];
+	sum += line[2];
+	if (line[0] == 'r' && line[1] == 'b')
+		sum += 1115;
+	line_sum(sum, a, b);
 }
 
 int	main(int ac, char **av)
@@ -67,7 +110,7 @@ int	main(int ac, char **av)
 	line = get_next_line(0);
 	while (line)
 	{
-		execute_instruction(line, &a, &b);
+		do_instruction(line, &a, &b);
 		free(line);
 		line = get_next_line(0);
 	}
